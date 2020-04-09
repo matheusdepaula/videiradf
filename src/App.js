646ddaaa9da5238d276bd80877a16a7a5363d974
Matheus de Paula <matheusdepaula.bsb@gmail.com>
2020-04-09 18:45:44 -0300

    changes in theme

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
 import LandingPage from './components/landingPage';
 
 class App extends Component {
   render() {
     return (
-      <div id="colorlib-page">
-        <div id="container-wrap">
-          <div id="colorlib-main">
-            <LandingPage/>
-          </div>
-        </div>
-      </div>
+      <LandingPage/>
     );
   }
 }
